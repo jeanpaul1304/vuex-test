c68dd99c7e8f12f5cf237b90d7f9599c46308ddb
jp1304 <jdiazvi@proveedor.intercorp.com.pe>
2017-04-06 11:41:43 -0500

    first-commit

---
+++ src/components/IncrementButton.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <button @click.prevent="activate">+1</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    activate () {
+      console.log('+1 Pressed')
+    }
+  }
+}
+</script>
+
+<style>
+</style>
